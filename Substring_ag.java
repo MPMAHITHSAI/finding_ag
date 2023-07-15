851d52ff3788893dd24c5c97fb9d5529e5cfa2c9
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-15 21:48:08 +0530

    ag string

---
+++ Substring_ag.java
@@ -0,0 +1,29 @@
+package Subseq;
+
+import java.util.Scanner;
+
+public class Substring_ag {
+	public static int seq(String str) {
+		int result=0;
+		int a_count = 0;
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i) == 'a') {
+				a_count++;
+			}
+			else if(str.charAt(i) == 'g') {
+				result +=a_count;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int value=seq(str);
+		System.out.println(value);
+
+	}
+
+}
